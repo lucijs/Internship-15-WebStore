295fb9e30bc79ebf16034e15ce5fa281e8145d80
lucijs <fradelic.lucija@gmail.com>
2024-03-10 22:45:42 +0100

    creating project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import ProductsPage from "./pages/ProductsPage";
+import ProductPage from "./pages/ProductPage";
+import Navigation from "./components/Navigation";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ProductsPage />
+      <ProductPage />
+      <Navigation />
+      <NotFoundPage />
+    </>
+  );
 }
 
 export default App;
